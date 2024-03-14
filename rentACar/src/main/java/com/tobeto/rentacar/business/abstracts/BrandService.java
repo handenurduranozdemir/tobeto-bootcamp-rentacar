bac2822844f7104e815b82e36e87b87ac67f2890
Handenur Duran Özdemir <119211532+handenurduranozdemir@users.noreply.github.com>
2024-03-14 23:34:32 +0300

    Initial commit

---
+++ rentACar/src/main/java/com/tobeto/rentacar/business/abstracts/BrandService.java
@@ -0,0 +1,4 @@
+package com.tobeto.rentacar.business.abstracts;
+
+public interface BrandService {
+}
---
+++ rentACar/src/main/java/com/tobeto/rentacar/business/concretes/BrandManager.java
@@ -0,0 +1,6 @@
+package com.tobeto.rentacar.business.concretes;
+
+import com.tobeto.rentacar.business.abstracts.BrandService;
+
+public class BrandManager implements BrandService {
+}
