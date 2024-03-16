25cd04d112e4623919b311143bb07ca19638e633
handenurduranozdemir <119211532+handenurduranozdemir@users.noreply.github.com>
2024-03-16 03:14:37 +0300

    Tüm entityler için controller sınıfları oluşturuldu

---
+++ src/main/java/com/tobeto/rentacar/api/controllers/ModelsController.java
@@ -0,0 +1,32 @@
+package com.tobeto.rentacar.api.controllers;
+
+import com.tobeto.rentacar.business.abstracts.ModelService;
+import com.tobeto.rentacar.business.dtos.request.CreateModelRequest;
+import com.tobeto.rentacar.business.dtos.responses.CreatedModelResponse;
+import com.tobeto.rentacar.business.dtos.responses.GetAllModelResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/models")
+public class ModelsController {
+
+    private ModelService modelService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreatedModelResponse add(@Valid @RequestBody CreateModelRequest createModelRequest) {
+        return modelService.add(createModelRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<GetAllModelResponse> getAll() {
+        return modelService.getAll();
+    }
+}
